ca46113b55ad4a1980f75b719bbe14a0058d5930
otherPOM <otherPOM@gmail.com>
2020-12-23 19:45:04 +0300

    Stage 2 done

--- Main.java
+++ Main.java
@@ -1,36 +1,42 @@
 package bullscows;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
+
+    private static int bulls = 0;
+    private static int cows = 0;
+    private static final String CODE = "9305";
+
     public static void main(String[] args) {
-        System.out.println("The secret code is prepared: ****.\n" +
-                "\n" +
-                "Turn 1. Answer:\n" +
-                "1234\n" +
-                "Grade: 1 cow.\n" +
-                "\n" +
-                "Turn 2. Answer:\n" +
-                "5678\n" +
-                "Grade: 1 cow.\n" +
-                "\n" +
-                "Turn 3. Answer:\n" +
-                "9012\n" +
-                "Grade: 1 bull and 1 cow.\n" +
-                "\n" +
-                "Turn 4. Answer:\n" +
-                "9087\n" +
-                "Grade: 1 bull and 1 cow.\n" +
-                "\n" +
-                "Turn 5. Answer:\n" +
-                "1087\n" +
-                "Grade: 1 cow.\n" +
-                "\n" +
-                "Turn 6. Answer:\n" +
-                "9205\n" +
-                "Grade: 3 bulls.\n" +
-                "\n" +
-                "Turn 7. Answer:\n" +
-                "9305\n" +
-                "Grade: 4 bulls.\n" +
-                "Congrats! The secret code is 9305.\n");
+        var guess = scan.nextLine();
+        grade(guess);
+        printGrade();
+    }
+
+    private static void grade(String guess) {
+        for (int i = 0; i < 4; i++) {
+            if (CODE.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else if (CODE.contains(guess.charAt(i) + "")) {
+                cows++;
+            }
+        }
+    }
+
+    private static void printGrade() {
+        var sb = new StringBuilder("Grade: ");
+        if (bulls == 0 && cows == 0) {
+            sb.append("None.");
+        } else if (cows == 0) {
+            sb.append(bulls).append(" bull(s).");
+        } else if (bulls == 0) {
+            sb.append(cows).append(" cow(s).");
+        } else {
+            sb.append(bulls).append(" bull(s) and ").append(cows).append(" cow(s).");
+        }
+        sb.append(" The secret code is ").append(CODE).append('.');
+        System.out.println(sb);
     }
 }
--- README.md
+++ README.md
@@ -6,3 +6,11 @@ In this stage, your program should:
 * Print a predefined game log that contains at least two turns.
 * The output to be graded is a 4-digit code consisting of unique digits.
 * The last line of your output contains a secret number.
+
+## Stage 2
+In this stage, your goal is to write the core part of the game: the grader.
+
+1. Your program should take a 4-digit number as an input.
+2. Use a predefined 4-digit code and grade the answer that was input. You can do it digit by digit.
+The grade is considered correct if it contains number-and-word pairs (like `X bulls and Y cows`) that give the correct information. If the answer doesn't contain any bulls and cows, you should output `None`.
+   
